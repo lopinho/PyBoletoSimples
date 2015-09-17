a068ea75bcb0032ba3e4c330809cd647e9e91d48
Andre Ferreira <lopinho@gmail.com>
2015-09-17 19:24:24 -0300

    parser para json que sabe lidar com date,datetime,time,decimal

---
+++ boletosimples/utils.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import json
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith('+00:00'):
+                r = r[:-6] + 'Z'
+            return r
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.time):
+            if is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(o, decimal.Decimal):
+            return str(o)
+        else:
+            return super(JSONEncoder, self).default(o)
