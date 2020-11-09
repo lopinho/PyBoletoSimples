eb16accba9d1f27c328384db0a3dfbd3c4811da1
André Ferreira <lopinho@gmail.com>
2020-11-09 15:36:03 -0300

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='lopinho@gmail.com',
     description='Library to use BoletoSimpes.com.br',
     license='MIT',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/lopinho/PyBoletoSimples',
     packages=['boletosimples'],
     classifiers=[
